84f7dd6c4457ad0df2e928ca9834cc427c98e4a7
liuy37 <2268980893@qq.com>
2024-04-12 17:49:44 +0800

    公司2024-04-12

--- 京东消费者数据/send_email.py
+++ 京东消费者数据/send_email.py
@@ -12,6 +12,7 @@
 """
 import datetime
 import smtplib
+from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
@@ -28,6 +29,8 @@ class AutoMail:
 
     # 发送邮件
     def send_mail(self, received_mail, email):
+        email['From'] = self.username_sendmail
+        email['To'] = ";".join(received_mail)
         try:
             smtp = smtplib.SMTP_SSL(self.mailserver, port=self.port)
             smtp.login(self.username_sendmail, self.username_authcode)
@@ -37,7 +40,7 @@ class AutoMail:
         except Exception as e:
             print(f"{datetime.date.today()}日邮件发送失败", e)
 
-    # 添加图片附件
+    # 添加图片附件到html模板中
     @staticmethod
     def add_img_file(img_path, image_id):
         file = open(img_path, "rb")
@@ -47,61 +50,79 @@ class AutoMail:
         img.add_header('Content-ID', '<image{}>'.format(image_id))
         return img
 
-    # 发送普通邮件
-    def mail_text(self, text, title, received_mail):
-        """
-        此方法是自动发送文本类文件的方法，需要传入发送的文本内容：str类型，文本的标题：str类型，接收的邮箱：list类型
-        :return:
-        """
-        email = MIMEText(text, 'plain', 'utf-8')
-        email["Subject"] = title
-        email['From'] = self.username_sendmail
-        email['To'] = ";".join(received_mail)
-        self.send_mail(received_mail, email)
+    #  添加附件(xlsx\txt\csv\word)等
+    @staticmethod
+    def add_file_attachment(file_path, file_name):
+        file = open(file_path, "rb")
+        file_data = file.read()
+        file.close()
+        file_attachment = MIMEApplication(file_data)
+        file_attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
+        return file_attachment
 
-    # 发送图文混合的html类型邮件
-    def mail_html(self, html_file, title, img_path, received_mail):
-        """
-        :param html_file: 这是一个html模板文件，需要传入文件路径
-        :param title: 这是邮件的标题
-        :param img_path: 这是编写html模板需要用到的图片的路径和图片的编号
-        :param received_mail: 这是需要接收邮件的邮箱列表
-        :return: 直接发送每日报告的html文件
-        """
-        # 1、生成可放文字+图片的容器
-        email = MIMEMultipart('related')  # 定义邮件的类型，related是超文本加附件的类型
-        content = MIMEText(html_file, 'html', 'utf-8')
-        email.attach(content)
-        email["Subject"] = title
-        email['From'] = self.username_sendmail
-        email['To'] = ";".join(received_mail)
-        # 添加图片附件
-        for path in img_path:
-            email.attach(self.add_img_file(path[0], path[1]))
-        # 发送邮件
-        self.send_mail(received_mail, email)
-
-    def send_email_schedule(self, html_file, title, img_path, received_mail):
-        def send_email():
-            self.mail_html(html_file, title, img_path, received_mail)
-        schedule.every(1).minutes.do(send_email)
+    @staticmethod
+    def timed_tasks(schedule_email):
+        """定时执行：一分钟执行一次"""
+        schedule.every(1).minutes.do(schedule_email)
         # 开始执行任务
         while True:
-            schedule.run_pending()
-            time.sleep(1)
+            schedule.run_pending()  # 运行所有可以运行的任务
+            time.sleep(1)  # 让程序暂停一秒钟
+
+    # 设置一个定时任务(1分钟执行一次发送文本类型邮件)
+    def send_text_email_schedule(self, text, title, received_mail):
+        """此方法是自动发送文本类文件的方法，需要传入发送的文本内容：str类型，文本的标题：str类型，接收的邮箱：list类型"""
+        def schedule_email():
+            email = MIMEText(text, 'plain', 'utf-8')
+            email["Subject"] = title
+            self.send_mail(received_mail, email)
+        # 执行定时任务
+        self.timed_tasks(schedule_email)
+
+    # 设置一个定时任务(1分钟执行一次：发送带附件的文本类型邮件)
+    def send_attachment_email_schedule(self, file_path, text, title, received_mail):
+        """
+        此方法是自动发送带附件的普通文件的方法，需要传入发送的文件路径：str类型，文件名：str类型，文本内容：str类型，文本的标题：str类型，接收的邮箱：list类型
+        :return:
+        """
+        def schedule_email():
+            email = MIMEText(text, 'plain', 'utf-8')
+            email["Subject"] = title
+            for path in file_path:
+                email.attach(self.add_file_attachment(path[0], path[1]))
+            self.send_mail(received_mail, email)
+        # 执行定时任务
+        self.timed_tasks(schedule_email)
+
+    # 设置一个定时任务(1分钟执行一次发送图文混合的html类型邮件)
+    def send_html_email_schedule(self, html_file, title, img_path, received_mail):
+        def schedule_email():
+            # 1、生成可放文字+图片的容器
+            email = MIMEMultipart('related')  # 定义邮件的类型，related是超文本加附件的类型
+            content = MIMEText(html_file, 'html', 'utf-8')
+            email.attach(content)
+            email["Subject"] = title
+            # 添加图片附件
+            for path in img_path:
+                email.attach(self.add_img_file(path[0], path[1]))
+            # 发送邮件
+            self.send_mail(received_mail, email)
+        # 执行定时任务
+        self.timed_tasks(schedule_email)
 
 
 def main():
     print("这是程序的入口")
     auto_mail = AutoMail()
-    # auto_mail.mail_text("薛之谦演唱会抢票时间：2024-04-10 17：17：00", "抢票提醒", ["1678865476@qq.com"])
+    # 发送文本邮件
+    # auto_mail.send_text_email_schedule("薛之谦演唱会抢票时间：2024-04-10 17：17：00", "抢票提醒", ["1678865476@qq.com"])
     # 发送图片邮件
     html_file = open("data/email_file/html_file/日报模板.txt", "r", encoding='utf-8').read()
     date_day = datetime.datetime.today().date()
     title = f"{date_day}日报"
     img_path = [["data/email_file/img_file/今日日报.png", 1], ["data/email_file/img_file/user_sex_pie_chart.png", 2]]
     received_mail = ["1678865476@qq.com"]
-    auto_mail.send_email_schedule(html_file, title, img_path, received_mail)
+    auto_mail.send_html_email_schedule(html_file, title, img_path, received_mail)
 
 
 if __name__ == '__main__':
